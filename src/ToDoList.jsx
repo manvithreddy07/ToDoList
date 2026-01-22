f75f0ff595c420339f6b4765cf306cf1054ab098
manvithreddy07 <manvithreddy.medagam@gmail.com>
2026-01-23 04:25:44 +0530

    Final Changes

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,7 +2,7 @@ import React,{useState} from 'react';
 
 function ToDoList(){
 
-    const[tasks,setTasks]=useState(["Eat BreakFast","Take a Shower","Walk your dog"]);
+    const[tasks,setTasks]=useState([""]);
     const[newTask,setNewTask]=useState("");
 
     function handleInputChange(event){
